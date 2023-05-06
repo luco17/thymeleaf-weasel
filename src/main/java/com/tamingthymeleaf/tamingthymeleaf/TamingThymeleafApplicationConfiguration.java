66f883c0db422b672c8f9ee64af0a1badb7ac1d8
luco17 <lucodonoghue@gmail.com>
2023-05-06 21:31:40 +0100

    refactored menus to Fragments

---
+++ src/main/java/com/tamingthymeleaf/tamingthymeleaf/TamingThymeleafApplicationConfiguration.java
@@ -0,0 +1,20 @@
+package com.tamingthymeleaf.tamingthymeleaf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class TamingThymeleafApplicationConfiguration {
+
+    @Bean
+    public ITemplateResolver svgTemplateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/svg/");
+        resolver.setSuffix(".svg");
+        resolver.setTemplateMode("XML");
+
+        return resolver;
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/main/resources/templates/**/*.html'],
+  content: [
+    "./src/main/resources/templates/**/*.html",
+    "./src/main/resources/templates/**/*.svg",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-green": "darkseagreen",
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
