d949fd13344d8e830b26e4c27aebd22bfd58c063
luco17 <lucodonoghue@gmail.com>
2023-05-11 21:21:37 +0100

    Create user form: client-side, controller, class

---
+++ src/main/java/com/tamingthymeleaf/application/user/web/CreateUserFormData.java
@@ -0,0 +1,86 @@
+package com.tamingthymeleaf.application.user.web;
+
+import com.tamingthymeleaf.application.user.CreateUserParameters;
+import com.tamingthymeleaf.application.user.Gender;
+import com.tamingthymeleaf.application.user.PhoneNumber;
+import com.tamingthymeleaf.application.user.UserName;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+public class CreateUserFormData {
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    private Gender gender;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
+    @NotBlank
+    @Pattern(regexp = "[0-9.\\-() x/+]+")
+    private String phoneNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public CreateUserParameters toParameters() {
+        return new CreateUserParameters(new UserName(firstName, lastName),
+                gender,
+                birthday,
+                new com.tamingthymeleaf.application.user.Email(email),
+                new PhoneNumber(phoneNumber));
+    }
+}
--- src/main/java/com/tamingthymeleaf/application/user/web/UserController.java
+++ src/main/java/com/tamingthymeleaf/application/user/web/UserController.java
@@ -1,5 +1,6 @@
 package com.tamingthymeleaf.application.user.web;
 
+import com.tamingthymeleaf.application.user.Gender;
 import com.tamingthymeleaf.application.user.UserService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.SortDefault;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -25,5 +28,12 @@ public class UserController {
     }) Pageable pageable) {
         model.addAttribute("users", service.getUsers(pageable));
         return "users/list";
+    }
+
+    @GetMapping("/create")
+    public String createUserForm(Model model) {
+        model.addAttribute("user", new CreateUserFormData());
+        model.addAttribute("genders", List.of(Gender.MALE, Gender.FEMALE, Gender.OTHER));
+        return "users/edit";
     }
 }
